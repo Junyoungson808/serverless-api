9c30b603f33b5620aec4788fd839d3682e513dab
Junyoung Son <junyoungson808@gmail.com>
2022-10-13 03:25:29 +0000

    update

--- handleRead/index.js
+++ handleRead/index.js
@@ -5,15 +5,29 @@ const dynamoose = require('dynamoose');
 
 // to be continued (code locally, zip, and upload);
 
+// Create a Schema
+const peopleSchema = new dynamoose.Schema({
+  id: String,
+  name: String,
+  phone: String
+});
+
+// Create a Model
+const peopleModel = dynamoose.model('people-model', peopleSchema);
 
 exports.handler = async (event) => {
-    
-    console.log('lord of the rings', event.queryStringParameters)
-    
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from HandleRead!'),
-    };
+  // TODO implement
+    console.log('lord of the rings', event.queryStringParameters);
+    const response = {statusCode: null, body: null};
+
+    try {
+      let peopleRecords = await peopleModel.scan().exec();
+      response.statusCode = 200;
+      response.body = JSON.stringify(peopleRecords);
+    } catch (e) {
+      console.log(e);
+      response.statusCode = 500;
+      response.body = JSON.stringify(e.message);
+    }
     return response;
 };
